d31b6806581cc14bd0ad903fae66f764da4332cd
michael bishop <michael.bishop@iohk.io>
2017-06-13 20:01:32 -0300

    some initial hydra based testing

--- jobsets/deployments.nix
+++ jobsets/deployments.nix
@@ -1,7 +1,17 @@
-{ pkgs ? (import <nixpkgs> {})}:
+{ pkgs ? (import <nixpkgs> {}), supportedSystems ? [ "x86_64-linux" ] }:
 
 with pkgs;
+with lib;
 
-rec {
+let
+  forAllSystems = genAttrs supportedSystems;
+  importTest = fn: args: system: import fn ({
+    inherit system;
+  } // args);
+  callTest = fn: args: forAllSystems (system: hydraJob (importTest fn args system));
+in rec {
   # TODO: tests of images
+  tests.boot = callTest ./boot.nix {};
+  tests.simpleNode = callTest ./simple-node.nix {};
+  tests.simpleNodeNixOps = callTest ./simple-node-nixops.nix {};
 }
---
+++ jobsets/simple-node-nixops.nix
@@ -0,0 +1,21 @@
+import ./make-test.nix ({ pkgs, ... }: {
+  name = "boot";
+  nodes = {
+    machine = { config, pkgs, ... }: {
+      imports = [ ../modules/cardano-node.nix <nixops/nix/options.nix> <nixops/nix/resource.nix> ];
+      services.cardano-node = {
+        enable = true;
+        testIndex = 0;
+        initialPeers = [];
+        genesisN = 6;
+        autoStart = true;
+      };
+    };
+  };
+  testScript = ''
+    startAll
+    $machine->waitForUnit("cardano-node.service");
+    # TODO, implement sd_notify?
+    $machine->waitForOpenPort(3000);
+  '';
+})
